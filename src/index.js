4cbe7cb0feaa6ee84e2644505a97c3f9aab34021
Evgeniy Yakichuk <yak.gjen@gmail.com>
2018-09-15 11:24:55 +0300

    fourth task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+	let enterParams = equation.split(' ');
+	let params = [];
+	
+	enterParams.forEach(item => {
+		params.push(item);
+	});
+	
+	/* add all pameters if a == 1 */
+	if (enterParams.length < 9) {
+		params.unshift('*');
+		params.unshift('1');
+	}
+	
+	
+	let a = +params[0];
+	let b = +params[4];
+	let c = +params[8];
+	
+	if (params[3] === '-') {
+		b = -b;
+	}
+	if (params[7] === '-') {
+		c = -c;
+	}
+	
+	let discriminant = Math.pow(b, 2) - (4 * a * c);
+	
+	let x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+	let x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+	
+	let resultArr = [];
+	resultArr.push(Math.round(x1));
+	resultArr.push(Math.round(x2));
+	
+	resultArr.sort((a, b) => {
+		return a - b;
+	});
+	
+	return resultArr;
+	
 }
